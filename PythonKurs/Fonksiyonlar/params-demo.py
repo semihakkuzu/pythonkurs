de994c5cad2da319a23056e13021a0d93d125398
semihakkuzu <126752920+semihakkuzu@users.noreply.github.com>
2023-03-02 09:42:23 +0300

    Add files via upload

---
+++ PythonKurs/Fonksiyonlar/params-demo.py
@@ -0,0 +1,14 @@
+# Kendisine gönderilen 2 sayıdan büyüğü bulan fonksiyonu yazın
+
+
+
+# Kendisine gönderilen bir string bilgi içinde her karakter kaçar kez tekrarlanmış bulunuz.
+
+
+
+# Kendisine gönderilen list,command,position ve value bilgilerine göre liste üzerinde güncelleme yap
+    # [1,2,3], ("add, remove"), ("beginning end")
+    # 
+
+
+
